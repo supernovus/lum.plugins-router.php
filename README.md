31207e6faf8f0c0f67ffbd67d6f54c7574177325
Timothy Totten <2010@huri.net>
2022-07-22 13:09:27 -0700

    Router core plugin

---
+++ README.md
@@ -0,0 +1,32 @@
+# lum.plugins-router.php
+
+## Summary
+
+A Core plugin for the `Lum\Router` class.
+
+Provides backwards compatibility with the original plugin.
+
+## Namespace
+
+`Lum\Plugins`
+
+### Classes
+
+| Name                    | Description                                       |
+| ----------------------- | ------------------------------------------------- |
+| Lum\Plugins\Router      | Plugin version of the Router.                     |
+
+## Official URLs
+
+This library can be found in two places:
+
+ * [Github](https://github.com/supernovus/lum.plugins-router.php)
+ * [Packageist](https://packagist.org/packages/lum/lum-plugins-router)
+
+## Authors
+
+- Timothy Totten
+
+## License
+
+[MIT](https://spdx.org/licenses/MIT.html)
---
+++ lib/Router.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lum\Plugins;
+
+use Lum\Router as LR;
+
+class Router extends LR\Router
+{
+  public $default_placeholder = LR\Vars::MODERN_PLACEHOLDER;
+
+  public $route_methods = 
+  [
+    'GET','POST','PUT','DELETE','HEAD','PATCH','OPTIONS',
+    'POKE','CLEAN','UNDELETE',
+  ];
+
+  public function __construct($opts=[])
+  {
+    parent::__construct($opts);
+
+    if (isset($opts['extend']) && $opts['extend'])
+    { // Register some helpers into the Lum object.
+      $core = \Lum\Core::getInstance();
+      $core->addMethod('dispatch',     [$this, 'route']);
+      $core->addMethod('addRoute',     [$this, 'add']);
+      $core->addMethod('addRedirect',  [$this, 'redirect']);
+      $core->addMethod('addPage',      [$this, 'display']);
+      $core->addMethod('setDefault',   [$this, 'setDefault']);
+      $core->addMethod('loadRoutes',   [$this, 'load']);
+    }
+  }
+}
